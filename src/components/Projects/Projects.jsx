752915b396bb14eceb96e257dd48943d0074f840
unknown <karpliuksasha@gmail.com>
2023-04-13 14:16:24 +0300

    change cards icon css

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,11 @@
 import { useInView } from 'react-intersection-observer';
 import '../Projects/Projects.css';
-import memory from '../../assets/Memory/memoryDop.svg';
+import star from '../../assets/Memory/Star.svg';
+import rectangle from '../../assets/Memory/Rectangle.svg';
+import polygon from '../../assets/Memory/Polygon.svg';
+import ellipse from '../../assets/Memory/Ellipse.svg';
 import weather from '../../assets/Weather/weatherDop.svg';
-import blog from '../../assets/Blog/blogicon.jpg'
+import blog from '../../assets/Blog/blogDop.svg'
 // import player from '../../assets/Blog/';
 import { Link } from 'react-router-dom'
 
@@ -21,23 +24,33 @@ function Projects() {
 					<h2>My projects</h2>
 				</div>
 				<div className="projects-cards">
-
 					<Link to='/memory'>
 						<div className={inView ? 'card memory activeCard' : 'card memory'}>
 							<h2>Memory Game</h2>
-							<img src={memory} alt="img" />
+							<div className="card-i-container">
+								<div className="m-container"><img src={rectangle} alt="img" /></div>
+								<div className="m-container"><img src={star} alt="img" /></div>
+								<div className="m-container"><img src={ellipse} alt="img" /></div>
+								<div className="m-container"><img src={polygon} alt="img" /></div>
+							</div>
+
 						</div>
 					</Link>
 					<Link to='/weather'>
 						<div className={inView ? 'card weather activeCard' : 'card weather'}>
 							<h2>Weather Widget</h2>
-							<img src={weather} alt="img" />
+							<div className="card-i-container">
+								<div className="w-container"><img src={weather} alt="img"/></div>
+							</div>
+
 						</div>
 					</Link>
 					<Link to='/blog'>
 						<div className={inView ? 'card player activeCard' : 'card player'}>
 							<h2>Blog</h2>
-							<img src={blog} alt="img" />
+							<div className="card-i-container">
+								<div className="w-container"><img src={blog} alt="img" /></div>
+							</div>
 						</div>
 					</Link>
 				</div>
